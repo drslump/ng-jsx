c040bcf0ab2e44e943972d5fa8564879631ec99e
DrSlump <drslump@pollinimini.net>
2014-12-12 17:21:21 +0100

    basically the whole syntax is supported

--- index.js
+++ index.js
@@ -11,38 +11,48 @@ function toCode (ast) {
     return recast.print(ast).code;
 }
 
-
 recast.run(function(ast, callback) {
+    var elementsCounter = 0;
+
     recast.visit(ast, {
 
         visitXJSElement: function (path) {
-            this.traverse(path);
 
-            var html = toCode(path.value);
-            return builders.literal(html);
+            elementsCounter++;
+            this.traverse(path);
+            elementsCounter--;
+
+            // Only wrap as a string the root node
+            if (0 === elementsCounter) {
+                var html = toCode(path.value);
+                return builders.literal(html);
+            }
         },
+
+        // TODO: Handle regular expressions ?
 
         visitXJSExpressionContainer: function (path) {
 
             var html,
                 expr = path.value.expression;
 
+            html = toCode(expr);
+
             // Support `template literals` for arbitrary syntax
             // TODO: Convert ES6 interpolation to the angular one?
             if (named.TemplateLiteral.check(expr)) {
-                html = toCode(expr);
                 html = html.substring(1, html.length - 1);
-            } else {
-                this.traverse(path);
-                html = toCode(expr);
             }
 
             // When not assigned to an attribute use angular interpolation
             if (!named.XJSAttribute.check(path.parentPath.value)) {
                 html = '{{ ' + html + ' }}';
+            } else {
+                html = '"' + html + '"';
             }
 
-            return builders.literal(html);
+            return html;
+            //return builders.literal(html);
         }
 
     });
